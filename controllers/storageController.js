413080bcb00501c240df62e96edf7e37f4864ec6
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2025-03-20 20:09:59 +0000

    update controller to use new views

--- controllers/storageController.js
+++ controllers/storageController.js
@@ -1,19 +1,50 @@
+const prisma = require("../config/prisma");
 const multer = require("multer");
-const upload = multer({ dest: "./uploads" });
 
-const multerUpload = upload.single("uploaded_file")
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname + "-" + uniqueSuffix);
+  },
+});
 
-async function getUploadForm(req, res) {
-  res.render("forms/upload-form");
+// const upload = multer({ dest: "./uploads" });
+
+const upload = multer({ storage });
+
+const multerUpload = upload.single("uploaded_file");
+
+function getFileForm(req, res) {
+  res.render("forms/upload-file-form");
 }
 
-async function uploadData(req, res) {
-  console.log(req.file, req.body)
-  res.redirect("/storage")
+function getFolderForm(req, res) {
+  res.render("forms/upload-folder-form");
+}
+
+function addFile(req, res) {
+  console.log(req.file, req.body);
+  res.redirect("/storage/file");
+}
+
+function addFolder(req, res) {
+  prisma.folder
+  res.redirect("/storage/folder");
+}
+
+
+
+function getStorageItems(req, res) {
+  res.render("pages/home-page");
 }
 
 module.exports = {
-  getUploadForm,
   multerUpload,
-  uploadData,
+  addFile,
+  getFileForm,
+  getFolderForm,
+  getStorageItems,
 };
--- routes/storageRouter.js
+++ routes/storageRouter.js
@@ -1,12 +1,16 @@
-const { Router } = require("express")
-const storageRouter = Router()
-const storageController = require("../controllers/storageController")
+const { Router } = require("express");
+const storageRouter = Router();
+const storageController = require("../controllers/storageController");
 
+
+storageRouter.route("/").get(storageController.getStorageItems)
+
+storageRouter.route("/folder").get(storageController.getFolderForm);
+// .post(storageController.multerUpload, storageController.upladData);
 
 storageRouter
-  .route("/")
-  .get(storageController.getUploadForm)
-  .post(storageController.multerUpload, storageController.uploadData);
+  .route("/file")
+  .get(storageController.getFileForm)
+  .post(storageController.multerUpload, storageController.addFile);
 
-
-module.exports = storageRouter
+module.exports = storageRouter;
